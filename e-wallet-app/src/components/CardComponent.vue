1fce657e9cbde8136c81ed4d9ae277f67a15bbc0
codeninjah <codeninjahblog@gmail.com>
2021-01-22 22:40:04 +0100

    Progress

--- e-wallet-app/src/components/CardComponent.vue
+++ e-wallet-app/src/components/CardComponent.vue
@@ -1,8 +1,14 @@
+
+
 <template>
     <div class="card-body">
+        <div class="vendor"><img src="../assets/vendor-evil.svg"></div>
+        <div class="chip"><img src="../assets/chip-dark.svg"></div>
         <div class="card-number">{{cardNumber}}</div>
-        <div class="card-holder">{{cardHolder}}</div>
-        <div class="expire">{{month}} / {{year}}</div>
+        <div class="name-date">
+            <div class="card-holder">{{cardHolder}}</div>
+            <div class="expire">{{month}} / {{year}}</div>
+        </div>
     </div>
 </template>
 
@@ -14,6 +20,21 @@ export default {
 </script>
 
 
-<style>
+<style scoped>
+    .card-body {
+        background-color: red;
+        width: 670px;
+        line-height: 3;
+        color: white;
+        padding: 25px;
+        display: flex;
+        flex-direction: column;
+    }
+
+    .vendor {
+        justify-content: flex-end;
+        align-items: flex-start;
+    }
 
 </style>
+
