88b0b22a2b062babf4b5c56e634b9f61b3ba364a
codeninjah <codeninjahblog@gmail.com>
2021-01-22 21:08:57 +0100

    Step 1: make it run
    Success

--- e-wallet-app/src/views/Home.vue
+++ e-wallet-app/src/views/Home.vue
@@ -2,17 +2,34 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+
+    <Card />
+    <Cardform />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
+import Card from '@/components/Card.vue'
+import Cardform from '@/components/Cardform.vue'
 
 export default {
-  name: 'Home',
+
+  data() {
+    return{
+      name: 'Home',
+    }
+  },
   components: {
-    HelloWorld
+    HelloWorld,
+    Card,
+    Cardform
   }
 }
 </script>
+
+
+<style>
+
+</style>
