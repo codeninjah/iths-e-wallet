f0538ed7a8e18de8a499c477ec01b72e915ad9f7
codeninjah <codeninjahblog@gmail.com>
2021-01-28 11:39:20 +0100

    Begin working with add ned card form

--- e-wallet-app/src/views/NewCard.vue
+++ e-wallet-app/src/views/NewCard.vue
@@ -1,6 +1,30 @@
 <template>
     <div class="form">
-        <p>Det här är new card</p>
+        <form>
+            <label for="card-nr">CARD NUMBER</label>
+            <br />
+            <input type="text" id="card-nr" maxlength="10" placeholder="XXXX XXXX XXXX XXXX">
+            <br />
+            <br />
+            <label for="name">CARDHOLDER NAME</label>
+            <br />
+            <input type="text" id="name" placeholder="Firstname Lastname">
+            <br />
+            <br />
+            <label>MONTH</label>
+            <label>YEAR</label>
+            <br />
+            <input type="text" placeholder="mtest">
+            <input type="text" placeholder="ytest">
+            <br />
+            <br />
+            <label>VENDOR</label>
+            <br />
+            <input type="text" placdeholder="vendor">
+            <br />
+            <br />
+            <input type="submit" id="submit" value="ADD CARD">
+        </form>
     </div>
 </template>
 
@@ -17,7 +41,37 @@ export default {
 
 
 
-<style>
+<style scoped>
 
+    form {
+        margin-left: 300px;
+        text-align: left;
+    }
+
+    #card-nr {
+        width: 380px;
+        border: 2px solid black;
+        border-radius: 5px;
+        font-size: 20px;
+        height: 40px;
+        padding-left: 15px;
+    }
+
+    #name {
+        width: 380px;
+        border: 2px solid black;
+        border-radius: 5px;
+        font-size: 20px;
+        height: 40px;
+        padding-left: 15px;
+    }
+
+    #submit {
+        width: 380px;
+        height: 50px;
+        font-size: 32px;
+        border: 2px solid black;
+        border-radius: 5px;
+    }
 
 </style>
