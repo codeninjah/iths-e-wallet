7ca6b46fdce51b3751cd15ca084af34781bca6d6
codeninjah <codeninjahblog@gmail.com>
2021-01-28 12:23:34 +0100

    More form styling

--- e-wallet-app/src/views/NewCard.vue
+++ e-wallet-app/src/views/NewCard.vue
@@ -11,19 +11,52 @@
             <input type="text" id="name" placeholder="Firstname Lastname">
             <br />
             <br />
-            <label>MONTH</label>
-            <label>YEAR</label>
+            <label for="month" id="month-label">MONTH</label>
+            <label for="year" id="year-label">YEAR</label>
             <br />
-            <input type="text" placeholder="mtest">
-            <input type="text" placeholder="ytest">
+            <select id="month">
+                <option value="01">01</option>
+                <option value="02">02</option>
+                <option value="03">03</option>
+                <option value="04">04</option>
+                <option value="05">05</option>
+                <option value="06">06</option>
+                <option value="07">07</option>
+                <option value="08">08</option>
+                <option value="09">09</option>
+                <option value="10">10</option>
+                <option value="11">11</option>
+                <option value="12">12</option>
+            </select>
+            <select id="year">
+                <option value="20">20</option>
+                <option value="21">21</option>
+                <option value="22">22</option>
+                <option value="23">23</option>
+                <option value="24">24</option>
+                <option value="25">25</option>
+                <option value="26">26</option>
+            </select> 
             <br />
             <br />
-            <label>VENDOR</label>
+            <label for="vendor">VENDOR</label>
             <br />
-            <input type="text" placdeholder="vendor">
+
+            <select id="vendor">
+                <option value="Bitcoin">Bitcoin Inc</option>
+                <option value="Blockchain">Blockchainn Inc</option>
+                <option value="EvilCorp">Evil Corp</option>
+                <option value="NinjaBank">Ninja Bank</option>
+            </select>
+
+            <br />
+            <br />
             <br />
             <br />
             <input type="submit" id="submit" value="ADD CARD">
+            <br />
+            <br />
+            <input type="button" id="home" value="BACK TO CARDS">
         </form>
     </div>
 </template>
@@ -46,10 +79,11 @@ export default {
     form {
         margin-left: 300px;
         text-align: left;
+        width: 380px;
     }
 
     #card-nr {
-        width: 380px;
+        width: 360px;
         border: 2px solid black;
         border-radius: 5px;
         font-size: 20px;
@@ -58,7 +92,7 @@ export default {
     }
 
     #name {
-        width: 380px;
+        width: 360px;
         border: 2px solid black;
         border-radius: 5px;
         font-size: 20px;
@@ -72,6 +106,58 @@ export default {
         font-size: 32px;
         border: 2px solid black;
         border-radius: 5px;
+        background-color: white;
+    }
+
+    #month {
+        width: 185px;
+        height: 50px;
+        font-size: 32px;
+        border: 2px solid black;
+        border-radius: 5px;
+        padding-left: 15px;
+    }
+
+    #year-label {
+        margin-left: 36%
+    }
+
+    #year {
+        margin-left: 10px;
+        width: 185px;
+        height: 50px;
+        font-size: 32px;
+        border: 2px solid black;
+        border-radius: 5px;
+        padding-left: 15px;
+    }
+
+    #vendor {
+        width: 380px;
+        height: 50px;
+        font-size: 32px;
+        border: 2px solid black;
+        border-radius: 5px;
+        padding-left: 15px;
+    }
+
+    #home {
+        width: 380px;
+        height: 50px;
+        font-size: 32px;
+        border: 2px solid black;
+        border-radius: 5px;
+        background-color: white;
+    }
+
+    #submit:hover {
+        background-color: grey;
+        cursor: pointer;
+    }
+
+    #home:hover {
+        background-color: grey;
+        cursor: pointer;
     }
 
 </style>
