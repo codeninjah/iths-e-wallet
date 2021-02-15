5fd10771f560be6b9fe35096f8b680bd34dd04b2
codeninjah <codeninjahblog@gmail.com>
2021-02-15 12:50:35 +0100

    lite styleing

--- e-wallet-app/src/components/CardComponent.vue
+++ e-wallet-app/src/components/CardComponent.vue
@@ -3,17 +3,20 @@
     <div class="card-body" 
         v-bind:class="card.vendor" 
         v-on:click="$emit(testar(index))">
-        <div>
+        <div class="first-row">
             <p v-if="card.vendor == 'EvilCorp'"><img src="../assets/vendor-evil.svg"></p>
             <p v-else-if="card.vendor == 'Bitcoin'"><img src="../assets/vendor-bitcoin.svg"></p>
             <p v-else-if="card.vendor == 'NinjaBank'"><img src="../assets/vendor-ninja.svg"></p>
             <p v-else-if="card.vendor == 'Blockchain'"><img src="../assets/vendor-blockchain.svg"></p>
+            <div class="chip"><img src="../assets/chip-dark.svg"></div>
         </div>
+ 
 
-        <div class="chip"><img src="../assets/chip-dark.svg"></div>
-        <div class="card-number">{{card.cardNumber}}</div>
+        <div class="card-holder">{{card.cardName}}</div>
+
+        <div class="card-number">{{card.cardNr}}</div>
         <div class="name-date">
-            <div class="card-holder">{{card.cardName}}</div>
+            
             <div class="expire">{{card.month}} / {{card.year}}</div>
         </div>
         <div class="vendor">{{card.vendor}}</div>
@@ -56,16 +59,18 @@ export default {
 
 <style scoped>
     .card-body {
-        width: 670px;
+        width: 600px;
         line-height: 3;
         color: white;
         padding: 25px;
+        min-height: 300px;
+        max-height: 300px;
         display: flex;
         flex-direction: column;
     }
 
     .active-card {
-        width: 670px;
+        width: 600px;
         line-height: 3;
         color: white;
         padding: 25px;
@@ -80,18 +85,36 @@ export default {
 
     .EvilCorp {
         background-color: red !important;
+        border-radius: 20px;
+        color: white;
     }
 
     .Blockchain {
         background-color: purple !important;
+        border-radius: 20px;
+        color: white;
     }
 
     .Bitcoin {
         background-color: orange !important;
+        border-radius: 20px;
+        color: black;
+        font-weight: 400;
     }
 
     .NinjaBank {
         background-color: black !important;
+        border-radius: 20px;
+        color: white;
     }
+
+    .card-number {
+        font-size: 26px;
+    }
+
+    .first-row {
+        justify-direction: row;
+    }
+
 </style>
 
